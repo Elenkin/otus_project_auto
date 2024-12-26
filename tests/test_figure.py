f280c48117fb39e8e1ffb9b763a7b70d41e3968e
mcAlexon <mr.alexdemonp2003@mail.ru>
2024-12-26 09:38:45 +0300

    добавлена функция round для округляния значений при вычислении

--- tests/test_figure.py
+++ tests/test_figure.py
@@ -13,20 +13,20 @@ square = Square(55)
 
 list_for_area = pytest.mark.parametrize('figure, other_figure, expected_area',
                              [
-                                 (rectangle, triangle, 144.97767144908306),
-                                 (rectangle, circle, 5136.548245743669),
+                                 (rectangle, triangle, 145),
+                                 (rectangle, circle, 5137),
                                  (rectangle, square, 3135),
-                                 (square, circle, 8051.548245743669),
-                                 (square, triangle, 3059.977671449083)
+                                 (square, circle, 8052),
+                                 (square, triangle, 3060)
 
                              ])
 
 list_for_perimeter = pytest.mark.parametrize('figure, other_figure, expected_perimeter',
                              [
                                  (rectangle, triangle, 83),
-                                 (rectangle, circle, 305.32741228718345),
+                                 (rectangle, circle, 305),
                                  (rectangle, square, 274),
-                                 (square, circle, 471.32741228718345),
+                                 (square, circle, 471),
                                  (square, triangle, 249)
 
                              ])
@@ -43,14 +43,14 @@ class TestFigure:
     @list_for_area
     def test_check_add_area_figure(self, figure, other_figure, expected_area):
 
-        add_area_figure = figure.add_area(other_figure)
+        add_area_figure = round(figure.add_area(other_figure))
         assert add_area_figure == expected_area, \
             f'Сумма площадей фигур: {expected_area}, но получено {add_area_figure}'
 
     @list_for_perimeter
     def test_check_add_perimeter_figure(self, figure, other_figure, expected_perimeter):
 
-        add_perimeter_figure = figure.add_perimeter(other_figure)
+        add_perimeter_figure = round(figure.add_perimeter(other_figure))
         assert add_perimeter_figure == expected_perimeter, \
             f'Сумма периметров фигур: {expected_perimeter}, но получено: {add_perimeter_figure}'
 
